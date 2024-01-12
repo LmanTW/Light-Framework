26ec98602fda88396813cd8fc763e0ab67eb341f
LmanTW <82919705+LmanTW@users.noreply.github.com>
2024-01-12 20:58:59 +0800

    Debugging Light.setStyle

--- Light-Framework/Modules/Components.js
+++ Light-Framework/Modules/Components.js
@@ -20,7 +20,6 @@ function deleteComponent (id) {
 
 // Get Component ID From element
 function getComponentIdFromParent (element) {
-  console.log(element)
   if (element.getAttribute('light') === null) {
     if (element.parentNode === undefined) return
     else return getComponentIdFromParent(element.parentNode)
